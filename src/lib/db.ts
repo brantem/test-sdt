16cf8ee72911b334b1ee772c32abc4e612a9dac5
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2025-02-12 21:26:57 +0700

    Add more pragmas

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,8 @@ import Database from "better-sqlite3";
 export function init(filename = ":memory:") {
   const db = new Database(filename);
   db.pragma("journal_mode = WAL");
+  db.pragma("synchronous = NORMAL");
+  db.pragma("foreign_keys = ON");
   db.exec(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
