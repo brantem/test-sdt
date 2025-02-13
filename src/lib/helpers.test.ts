db27c054e045452c4d86f734de39d9a983cb0d66
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2025-02-13 15:25:51 +0700

    Add helpers.processInBatches

--- src/lib/helpers.test.ts
+++ src/lib/helpers.test.ts
@@ -1,6 +1,30 @@
 import * as helpers from "./helpers.js";
 
-test("isValidTimezone", () => {
-  expect(helpers.isValidTimezone("Asia/Jakarta")).toBeTruthy();
-  expect(helpers.isValidTimezone("a/b")).toBeFalsy();
+describe("helpers", () => {
+  test("isValidTimezone", () => {
+    expect(helpers.isValidTimezone("Asia/Jakarta")).toBeTruthy();
+    expect(helpers.isValidTimezone("a/b")).toBeFalsy();
+  });
+
+  describe("processInBatches", () => {
+    it("should process correctly", async () => {
+      const cb = vi.fn();
+      await helpers.processInBatches([1, 2, 3], 2, cb);
+      expect(cb).toHaveBeenCalledTimes(2);
+      expect(cb).toHaveBeenCalledWith([1, 2]);
+      expect(cb).toHaveBeenCalledWith([3]);
+    });
+
+    it("should stop if it receives an empty array", () => {
+      const cb = vi.fn();
+      helpers.processInBatches([], 1, cb);
+      expect(cb).not.toHaveBeenCalled();
+    });
+
+    it("should stop if it receives an empty size", () => {
+      const cb = vi.fn();
+      helpers.processInBatches([1], 0, cb);
+      expect(cb).not.toHaveBeenCalled();
+    });
+  });
 });
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -14,3 +14,10 @@ export function isValidTimezone(timeZone: string | undefined) {
 export async function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export async function processInBatches<T>(arr: T[], n: number, cb: (arr: T[]) => Promise<void>) {
+  if (!arr.length || n <= 0) return;
+  for (let i = 0; i < arr.length; i += n) {
+    await cb(arr.slice(i, i + n));
+  }
+}
